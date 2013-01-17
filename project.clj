4a06c1a0437a2ead71d8d77c3f20ae9337d9b375
Brandon Bloom <brandon@brandonbloom.name>
2013-01-16 16:26:06 -0800

    Clojure 1.5 RC2

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
                  [factjor "0.1.0-SNAPSHOT"]
                  ;; Is it OK to depend on pallix's clojar?
                  [org.clojars.pallix/batik "1.7.0"]])
