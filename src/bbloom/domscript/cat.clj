569213e0bb878d7cfd4d0e87c9ce50b800004089
Brandon Bloom <brandon@brandonbloom.name>
2013-03-13 13:50:23 -0400

    Use cat-ops from Factjor

--- src/bbloom/domscript/cat.clj
+++ src/bbloom/domscript/cat.clj
@@ -1,35 +1,7 @@
 (ns bbloom.domscript.cat
   (:refer-clojure :exclude [remove])
   (:require [factjor.core :as cat :refer (defprim)]
-            [bbloom.domscript.core :as dom]))
-
-;;; Helper macros that belong in Factjor
-
-(defn parse-stack-effect [effect]
-  (let [[inputs [sep & outputs]] (split-with (complement '#{--}) effect)]
-    (assert (= sep '--) "Expected '-- in stack effect")
-    {:inputs (vec inputs) :outputs (vec outputs)}))
-
-(defn cat-op-form [f effect]
-  (let [{:keys [inputs outputs]} (parse-stack-effect effect)
-        input-set (set inputs)
-        return-values (clojure.core/remove input-set outputs)
-        result-sym (gensym)
-        output-map {(first return-values) result-sym}
-        conj-syms (map #(get output-map % %) outputs)]
-    (assert (<= (count return-values) 1)
-            (str "Unable to infer " f " output stack from: " effect))
-  `(defprim ~(-> f name symbol) ~inputs
-     (let [~result-sym (~f ~@inputs)]
-       ~(if (empty? conj-syms)
-          '$
-          `(conj ~'$ ~@conj-syms))))))
-
-(defmacro cat-ops [ns & op-specs]
-  (let [ns (name ns)]
-    `(do ~@(for [[nm effect] (partition 2 op-specs)]
-             (cat-op-form (symbol ns (name nm)) effect)))))
-
+            [factjor.interop :refer (cat-ops)]))
 
 (cat-ops dom
 
